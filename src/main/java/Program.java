3814e31b114b85cd54ae22ed2439b610d4a7c9d9
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2024-03-20 17:46:14 +0300

    Added the random team creating and enum of names for units

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -6,11 +6,16 @@ import Heroes.Shooters.Archer;
 import Heroes.Shooters.Crossbowman;
 import Heroes.Wizard.Magus;
 import Heroes.Wizard.Monk;
+import Resources.Names;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Program {
     public static void main(String[] args) {
+        int teamCapacity = 10;
+
+
         BasicHero kirill = new Monk("Kirill");
         BasicHero gandalf = new Magus("Gandalf");
         BasicHero solderJane = new Pikeman("Jane");
@@ -21,19 +26,6 @@ public class Program {
         BasicHero legionary = new Pikeman("Legionary");
         BasicHero bandit = new Robber("Bandit");
         BasicHero pleb = new Redneck("Pleb"); // Простолюдин/ Плебей
-
-        ArrayList<BasicHero> right = new ArrayList<>();
-        right.add(kirill);
-        right.add(gandalf);
-        right.add(solderJane);
-        right.add(robinGood);
-        right.add(johnD);
-        right.add(sniper);
-        right.add(sadMan);
-        right.add(legionary);
-        right.add(bandit);
-        right.add(pleb);
-
 
         BasicHero mifodiy = new Monk("Mifodiy");
         BasicHero merlin = new Magus("Merlin");
@@ -46,26 +38,79 @@ public class Program {
         BasicHero farmer = new Redneck("Farmer");
         BasicHero neanderthal = new Redneck("Neanderthal"); // Неандерталец
 
+        ArrayList<BasicHero> rightTeam = new ArrayList<>();
 
-        ArrayList<BasicHero> left = new ArrayList<>();
-        left.add(mifodiy);
-        left.add(merlin);
-        left.add(braveHeart);
-        left.add(artur);
-        left.add(curva);
-        left.add(greenArrow);
-        left.add(crossbowmanJr);
-        left.add(farmer);
-        left.add(neanderthal);
-        left.add(solovey);
+        rightTeam.add(kirill);
+        rightTeam.add(gandalf);
+        rightTeam.add(solderJane);
+        rightTeam.add(robinGood);
+        rightTeam.add(johnD);
+        rightTeam.add(sniper);
+        rightTeam.add(sadMan);
+        rightTeam.add(legionary);
+        rightTeam.add(bandit);
+        rightTeam.add(pleb);
+        rightTeam.add(merlin);
+        rightTeam.add(mifodiy);
+        rightTeam.add(braveHeart);
+        rightTeam.add(artur);
+        rightTeam.add(curva);
+        rightTeam.add(greenArrow);
+        rightTeam.add(crossbowmanJr);
+        rightTeam.add(farmer);
+        rightTeam.add(neanderthal);
+        rightTeam.add(solovey);
 
-        right.forEach(n -> n.getInfo()); // итерация списка и вызов метода каждого наследника
-        System.out.println("==".repeat(20));
-        left.forEach(n -> n.getInfo());
+
+        ArrayList<BasicHero> leftTeam = new ArrayList<>();
+        ArrayList<BasicHero> greenTeam = new ArrayList<>(10);
 
 
+        /**
+         * Рандомное распределение уже существующих персонажей по командам.
+         */
+        while (leftTeam.size() < rightTeam.size()) {
+            leftTeam.add(rightTeam.remove(new Random().nextInt(rightTeam.size())));
+        }
+
+        /**
+         * Рандомное создание новых персонажей и добавление их в команду.
+         * Используются рандомные имена из энумератора имен
+         */
+        for (int i = 0; i < teamCapacity; i++) {
+            int selectionCondition = new Random().nextInt(7);
+            switch (selectionCondition) {
+                case (0):
+                    greenTeam.add(new Pikeman(String.valueOf(Names.values()[new Random().nextInt(Names.values().length)])));
+                    break;
+                case (1):
+                    greenTeam.add(new Robber(String.valueOf(Names.values()[new Random().nextInt(Names.values().length)])));
+                    break;
+                case (2):
+                    greenTeam.add(new Archer(String.valueOf(Names.values()[new Random().nextInt(Names.values().length)])));
+                    break;
+                case (3):
+                    greenTeam.add(new Crossbowman(String.valueOf(Names.values()[new Random().nextInt(Names.values().length)])));
+                    break;
+                case (4):
+                    greenTeam.add(new Magus(String.valueOf(Names.values()[new Random().nextInt(Names.values().length)])));
+                    break;
+                case (5):
+                    greenTeam.add(new Monk(String.valueOf(Names.values()[new Random().nextInt(Names.values().length)])));
+                    break;
+                default:
+                    greenTeam.add(new Redneck(String.valueOf(Names.values()[new Random().nextInt(Names.values().length)])));
+            }
+
+        }
 
 
+        System.out.println("Right team");
+        rightTeam.forEach(n -> n.getInfo()); // итерация списка и вызов метода каждого наследника
+        System.out.println("Left team");
+        leftTeam.forEach(n -> n.getInfo());
+        System.out.println("Green team");
+        greenTeam.forEach(n -> n.getInfo());
 
 
     }
---
+++ src/main/java/Resources/Names.java
@@ -0,0 +1,24 @@
+package Resources;
+
+public enum Names {
+    Noah,
+    Liam,
+    William,
+    Mason,
+    James,
+    Benjamin,
+    Jacob,
+    Michael,
+    Elijah,
+    Ethan,
+    Emma,
+    Olivia,
+    Ava,
+    Sophia,
+    Isabella,
+    Mia,
+    Charlotte,
+    Abigail,
+    Emily,
+    Harper
+}
